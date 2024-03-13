9b03a3ec72f481af712807a1a011746243a6c212
nankap <nankapserge@gmail.com>
2024-03-13 19:30:03 +0100

    first commit

---
+++ src/main/java/com/logone/gestionstock/GestionStockApplication.java
@@ -0,0 +1,14 @@
+package com.logone.gestionstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+GestionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockApplication.class, args);
+	}
+
+}
